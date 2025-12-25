a8f20cd350fad8e474eef4d84eb63b9a587a135a
ahmadrodiii <ahmadrodi2007@gmail.com>
2025-12-25 22:29:14 +0700

    verceljson

--- api2/copyright.js
+++ api2/copyright.js
@@ -1,13 +1,10 @@
 // api2/copyright.js
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Cache-Control", "no-store");
 
+  
   res.status(200).json({
     success: true,
-    text: "© R STUDIO",
-    owner: "R STUDIO",
-    website: "https://rstudiolab.online",
-    updatedAt: new Date().toISOString(),
+    text: "© rstudiolab.online"
   });
 }
+;
