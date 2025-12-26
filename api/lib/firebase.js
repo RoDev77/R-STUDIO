6779dd8d87ce77fc6f9dd427c9db30bc26d0cb94
ahmadrodiii <ahmadrodi2007@gmail.com>
2025-12-26 09:22:32 +0700

    firebasee ganti

--- api/lib/firebase.js
+++ api/lib/firebase.js
@@ -1,21 +1,23 @@
 import admin from "firebase-admin";
 
-let firestore = null;
+let firestore;
 
 export function getFirestore() {
   if (firestore) return firestore;
 
   if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-    throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+    throw new Error("FIREBASE_SERVICE_ACCOUNT missing");
   }
 
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-  );
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
+      }),
     });
   }
 
